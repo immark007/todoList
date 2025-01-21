05463e9c00d90eab3e94fc9502274d655ae2077e
immark007 <immark5507@gmail.com>
2025-01-21 14:46:08 -0300

    feat: create repository User

---
+++ src/main/java/br/mark/github/todoList/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package br.mark.github.todoList.repository;
+
+
+import br.mark.github.todoList.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
