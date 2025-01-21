73dd968aad2b23e326b2e728c5b122de59bfc1c8
immark007 <immark5507@gmail.com>
2025-01-21 14:20:20 -0300

    feat: initial config

---
+++ src/test/java/br/mark/github/todoList/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mark.github.todoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
