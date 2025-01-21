5387bf938bf403f684561873fac8b0127e718dfb
immark007 <immark5507@gmail.com>
2025-01-21 20:51:31 -0300

    feat: Criando o controller da minha Todo

---
+++ src/main/java/br/mark/github/todoList/controller/TodoController.java
@@ -0,0 +1,9 @@
+package br.mark.github.todoList.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todo")
+public class TodoController {
+}
