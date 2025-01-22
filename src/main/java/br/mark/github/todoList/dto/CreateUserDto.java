f373af4ecb07370a5ebc36490f32cba62d19a5d9
immark007 <immark5507@gmail.com>
2025-01-22 07:25:23 -0300

    feat: adicionando validation

--- src/main/java/br/mark/github/todoList/dto/CreateUserDto.java
+++ src/main/java/br/mark/github/todoList/dto/CreateUserDto.java
@@ -1,6 +1,16 @@
 package br.mark.github.todoList.dto;
 
 
-public record CreateUserDto(String username, String email, String password)
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateUserDto(
+        @NotBlank
+        String username,
+        @NotBlank
+        String email,
+        @NotBlank
+        @Size(min = 8)
+        String password)
 {
 }
