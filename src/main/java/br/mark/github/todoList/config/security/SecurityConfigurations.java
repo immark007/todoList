ddb238070b5d232613de396f5df292ac6951072d
immark007 <immark5507@gmail.com>
2025-01-23 08:13:07 -0300

    feat: permissões do security para login

--- src/main/java/br/mark/github/todoList/config/security/SecurityConfigurations.java
+++ src/main/java/br/mark/github/todoList/config/security/SecurityConfigurations.java
@@ -6,12 +6,22 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
-public class SecurityConfigurations{
+public class SecurityConfigurations {
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers(HttpMethod.GET,"/users").permitAll().requestMatchers(HttpMethod.GET,"/users/*").permitAll());
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers(HttpMethod.GET, "/users").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/users/*").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login") // URL da sua página de login
+                        .permitAll()         // Permitir acesso à página de login para todos
+                )
+                .logout(logout -> logout.permitAll()); // Adiciona suporte para logout
 
         return http.build();
     }
