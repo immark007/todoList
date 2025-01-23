30ecf199859d4635f09f8739d65a5a4ae76cc535
immark007 <immark5507@gmail.com>
2025-01-23 11:59:31 -0300

    feat: autenticação com spring security

--- src/main/java/br/mark/github/todoList/config/security/SecurityConfigurations.java
+++ src/main/java/br/mark/github/todoList/config/security/SecurityConfigurations.java
@@ -1,13 +1,19 @@
 package br.mark.github.todoList.config.security;
 
+import br.mark.github.todoList.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfigurations {
+    @Autowired
+    private AuthenticationService authenticationService;
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
@@ -18,11 +24,16 @@ public class SecurityConfigurations {
                                 .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
-                        .loginPage("/login") // URL da sua página de login
+                        .loginPage("/login")// URL da sua página de login
                         .permitAll()         // Permitir acesso à página de login para todos
                 )
                 .logout(logout -> logout.permitAll()); // Adiciona suporte para logout
 
         return http.build();
     }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(authenticationService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
 }
--- src/main/java/br/mark/github/todoList/controller/UserController.java
+++ src/main/java/br/mark/github/todoList/controller/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<User> addUser(@RequestBody @Validated CreateUserDto createUserDto) {
         var userId = userService.createUser(createUserDto);
 
